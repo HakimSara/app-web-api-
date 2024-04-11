84d8176ebf299573f6148a04d1061c4c2891bb06
HakimSara <hakimsara145@gmail.com>
2024-04-11 17:42:24 -0300

    acesso a api json server

--- projeto-react-api-1-sara/src/pages/NovoLivro.js
+++ projeto-react-api-1-sara/src/pages/NovoLivro.js
@@ -1,38 +1,67 @@
+import { useState, useEffect } from 'react'
+import styles from './NovoLivro.module.css'
+import Input from '../Components/Form/input'
+import Select from '../Components/Select/select'
+function NovoLivro() {
+  const [categories, setCategories] = useState([])
 
-import styles from "./NovoLivro.module.css";
-import Input from "../Components/Form/input";
-function NovoLivro(){
-        return (
-            <section className={styles.novo_livro_container}>
-            <h1>Cadastre livro</h1>
-        
-            <form>
-                <Input
-                type="text"
-                name="nome_livro"
-                id="nome_livro"
-                placeholder="Digite o titulo do livro"
-                text="Digite o titulo do livro"
-                />
-        
-                <Input
-                type="text"
-                name="nome_autor"
-                id="nome_autor"
-                placeholder="Digite o nome do autor"
-                text="Digite o nome do autor"
-                />
-        
-                <Input
-                type="text"
-                name="descricao_livro"
-                id="descricao_livro"
-                placeholder="Digite a descricao do livro"
-                text="digite a descricao do livro"
-                />
-        </form>
-        
-        </section>
-    )
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'aplication/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data)
+        console.log(data)
+      })
+
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+  return (
+    <section className={styles.novolivros_container}>
+      <h1>Cadastre livro</h1>
+
+      <form>
+        <Input
+          type="text"
+          name="nome_livro"
+          id="nome_livro"
+          placeholder="Digite o titulo do livro"
+          text="Digite o titulo do livro"
+        />
+
+        <Input
+          type="text"
+          name="nome_autor"
+          id="nome_autor"
+          placeholder="Digite o nome do autor"
+          text="Digite o nome do autor"
+        />
+
+        <Input
+          type="text"
+          name="descricao_livro"
+          id="descricao_livro"
+          placeholder="Digite a descricao do livro"
+          text="digite a descricao do livro"
+        />
+
+        <Select
+          name="categoria-id"
+          text="selecione a categoria do livro"
+          options={categories}
+        />
+
+        <p>
+          <button type="submit">Enviar</button>
+        </p>
+      </form>
+    </section>
+  )
 }
-export default NovoLivro;
+export default NovoLivro
